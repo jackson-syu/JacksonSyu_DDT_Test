f46675a3339e9750fb7657a818955d13a4e93e69
jackson.syu <jackson.syu08854@gmail.com>
2020-09-24 16:37:05 +0800

    retrofit測試

---
+++ app/src/test/java/com/hikari/jacksonsyu_ddt_test/api/TestUnittest.kt
@@ -0,0 +1,41 @@
+package com.hikari.jacksonsyu_ddt_test.api
+
+import com.hikari.jacksonsyu_ddt_test.model.TestService
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+
+/**
+ * Created by hikari on 2020/9/24.
+ */
+class TestUnittest {
+
+    lateinit var latch: CountDownLatch
+    var json: String = ""
+    @Before
+    fun init() {
+        latch = CountDownLatch(1)
+    }
+
+    @Test
+    fun run() {
+        TestService.loadTest(object : TestService.CallBack{
+            override fun onSuccess(jsonString: String) {
+                json = jsonString
+                latch.countDown()
+            }
+
+            override fun onError(error: String) {
+                json = error
+                latch.countDown()
+            }
+
+        })
+
+
+        latch.await()
+
+        println(json)
+    }
+
+}
