6e68e991d3ac43ec948233042d45923fda520ec1
jackson.syu <jackson.syu08854@gmail.com>
2021-04-14 14:21:30 +0800

    photoview test

--- app/src/main/java/com/hikari/jacksonsyu_ddt_test/MainActivity.kt
+++ app/src/main/java/com/hikari/jacksonsyu_ddt_test/MainActivity.kt
@@ -3,11 +3,16 @@ package com.hikari.jacksonsyu_ddt_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import uk.co.senab.photoview.PhotoView
+import uk.co.senab.photoview.PhotoViewAttacher
 
 /**
  * Created by hikari on 2020/9/23
  */
 class MainActivity : AppCompatActivity() {
+
+    lateinit var photoview: PhotoView
 
     companion object {
         private const val TAG = "MainActivity"
@@ -18,9 +23,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         init()
+
+        initHandler();
     }
 
     fun init() {
         Log.d(TAG, "MainActivity~");
+
+        photoview = findViewById(R.id.main_photoview)
+
+    }
+
+    private fun initHandler() {
+        var attacher: PhotoViewAttacher = PhotoViewAttacher(photoview)
+        attacher.setOnViewTapListener(object : PhotoViewAttacher.OnViewTapListener{
+            override fun onViewTap(view: View?, x: Float, y: Float) {
+
+            }
+        })
     }
 }
